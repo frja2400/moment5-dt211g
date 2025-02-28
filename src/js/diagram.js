6f6a3f98d27211bc77ea9f2a50272c1ed4ad4bac
Frida Jansson <frja2400@student.miun.se>
2025-02-28 14:11:25 +0100

    Kommenterade och strukturerade koden samt adderade z-index för karta och diagram.

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,7 +1,9 @@
 "use strict";
 
 
+//Asynkron funktion som hämtar data från en URL och konverterar till JSON.
 async function fetchData() {
+    //Använder try-catch för eventuella fel.
     try {
         const response = await fetch('https://studenter.miun.se/~mallar/dt211g/');
         const data = await response.json();
@@ -13,15 +15,21 @@ async function fetchData() {
 }
 
 async function createBarChart() {
+
+    //Anropar fetchData och väntar på att datan ska bli tillgänglig. 
     const data = await fetchData();
 
+    //Filtrerar ut kurser från den hämtade datan.
     const courses = data.filter(course => course.type === 'Kurs');
 
+    //Sorterar kurserna i fallande ordning och tar de sex första objekten i den sorterade arrayen.
     const topCourses = courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal).slice(0, 6);
 
+    //Använder .map för att skapa nya arrayer av de filtrerade kurserna och antalet sökande.
     const courseNames = topCourses.map(course => course.name);
     const totalApplicants = topCourses.map(course => course.applicantsTotal);
 
+    //Skapar ett stapeldiagram med ApexChart.js.
     const options = {
         chart: {
             type: 'bar',
@@ -53,22 +61,30 @@ async function createBarChart() {
         }
     };
 
+    //Konfigurerar diagrammet med inställningarna jag implementerat i options.
     const chart = new ApexCharts(document.querySelector("#myChart"), options);
+    //Visar diagrammet i HTML-elementet myChart.
     chart.render();
 }
 
 createBarChart();
 
 async function createCircelChart() {
+
+    //Anropar fetchData och väntar på att datan ska bli tillgänglig. 
     const data = await fetchData();
 
+    //Filtrerar ut program från den hämtade datan.
     const programs = data.filter(program => program.type === 'Program');
 
+    //Sorterar programmen i fallande ordning och tar de 5 första objekten i den sorterade arrayen. 
     const topPrograms = programs.sort((a, b) => b.applicantsTotal - a.applicantsTotal).slice(0, 5);
 
+    //Använder .map för att skapa nya arrayer av de filtrerade programmen och antalet sökande.
     const programNames = topPrograms.map(program => program.name);
     const totalApplicants = topPrograms.map(program => program.applicantsTotal);
 
+    //Skapar ett cirkeldiagram med Chart.js.
     const ctx = document.getElementById('myChart2');
 
     new Chart(ctx, {
@@ -90,3 +106,4 @@ async function createCircelChart() {
 }
 
 createCircelChart();
+
---
+++ src/js/map.js
@@ -0,0 +1,55 @@
+"use strict";
+
+
+//Implementerar en karta med leaflet. 
+let map = L.map('map').setView([60.0, 18.0], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//Deklarerar en variabel utan värde, för användning senare.
+let marker;
+
+//Funktion för sökning av plats i karta.
+function searchLocation() {
+
+    //Hämtar värde från sökfält och lagrar det i en variabel
+    let query = document.getElementById('search').value;
+
+    /*Använder fetch för att hämta bas-URL för Nominatim API:s sökfunktion med parametrar som 
+    anger svaret i JSON-format och innehåller värdet från sökfältet.*/
+    fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            //Kontrollerar om det finns något resultat, om det finns objekt i data-arrayen.
+            if (data.length > 0) {
+                //Hämtar första latitud och longitud från data-arrayen och uppdaterar kartan med koordinaterna.
+                let lat = data[0].lat;
+                let lon = data[0].lon;
+                map.setView([lat, lon], 14);
+
+                //Ta bort markör om det redan finns.
+                if (marker) {
+                    map.removeLayer(marker);
+                }
+
+                //Lägg till marör på kartan med koordinaterna.
+                marker = L.marker([lat, lon]).addTo(map);
+            } else {
+                alert('Plats inte hittad');
+            }
+        })
+        //Fånga upp fel från fetch-anropet. 
+        .catch(error => console.error('Error:', error));
+}
+
+//Skapar en händelselyssnare som anropar SearchLocation vid trycker av Enter.
+document.getElementById('search').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        searchLocation();
+    }
+});
+
+window.searchLocation = searchLocation;
